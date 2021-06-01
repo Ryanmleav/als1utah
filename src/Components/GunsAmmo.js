b2098a50cfd7f8b9c82afb1ebccd07e0f65afed1
ryan <hopelessgains@gmail.com>
2021-06-01 15:35:12 -0600

    added guns and ammo page input box

--- src/Components/GunsAmmo.js
+++ src/Components/GunsAmmo.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, {Component} from "react";
+import axios from 'axios';
 import Header from "./Header";
 
-const GunsAmmo = () => {
+class GunsAmmo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: "",
+    };
+    this.submit = this.submit.bind(this);
+  }
+
+
+  submit() {
+    console.log(this.props);
+    axios
+      .post("/news/post", this.state)
+      .then(() => this.props.history.push("/news"))
+      .catch((err) => console.log(err));
+  }
+  render(){
   return (
     <div>
       <Header/>
+      <div className="form content-box">
+          <div className="form-main">
+            <div className="form-input-box"></div>
+            <div className="form-text-box">
+              <p className="newsfeed">Content:</p>
+              <textarea
+                value={this.state.content}
+                onChange={(e) => this.setState({ content: e.target.value })}
+              />
+            </div>
+          </div>
+          <button onClick={this.submit} className="dark-button">
+            Post
+          </button>
+        </div>
     </div>
   )
+}
 }
 export default GunsAmmo
